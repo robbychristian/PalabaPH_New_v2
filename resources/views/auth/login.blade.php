1dafb88bb029782cc2252de3011f3cf5c68cda33
robbychristian <robbychristiandeleon@gmail.com>
2022-05-04 15:36:42 +0800

    added welcome + login + tailwind

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,34 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+    <div class="flex flex-col h-[90vh] w-screen md:h-[85vh]">
+        <div class="grid grid-cols-1 h-full w-full md:grid-cols-2 bg-sky-300 ">
+            <div class="hidden md:flex justify-center items-center">
+                <div class="hidden md:flex justify-content">
+                    <img src="{{ asset('images/PalabaPH-Icon.png') }}" class="h-4/6 w-full" />
                 </div>
+            </div>
+            <div class="flex justify-center space-y-5 h-full w-full">
+                <form action="{{ route('login') }}" method="POST"
+                    class="flex flex-col h-4/5 w-full items-center bg-white md:h-full">
+                    @csrf
+                    <img src="{{ asset('images/PalabaPH-name-DARK 1.png') }}" class="h-1/10 w-1/2 mt-10" />
+                    <hr class="w-4/5 mt-4" style="height: 2px">
+                    <div class="text-black text-xl font-bold mt-10">Login Your Account</div>
+                    <input type="text" name="email" class="border-2 border-gray-400 h-10 rounded-md w-4/6 mt-10 px-2"
+                        placeholder="Username or Email">
+                    <input type="password" name="password" class="border-2 border-gray-400 h-10 rounded-md w-4/6 mt-10 px-2"
+                        placeholder="Password">
+                    <div class="flex flex-row space-x-2 mt-8 items-center">
+                        <input type="checkbox" name="remember">
+                        <div class="font-bold">Remember Me</div>
+                    </div>
+                    <div class="flex flex-row space-x-2 mt-6 items-center">
+                        <button class="bg-yellow-300 border-2 rounded-lg w-24 h-12 font-bold text-xl">Login</button>
+                        <a class="font-bold text-lg cursor-pointer" href="#">Forgot your password?</a>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
-</div>
 @endsection
