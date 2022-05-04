199b5b06b66ea276fe4b58bda69aa8f353fcc424
tinemanabs <christinemmanabat@gmail.com>
2022-05-05 02:31:54 +0800

    registration UI done

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,9 +1,208 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="h-screen w-full flex flex-col bg-sky-300 items-center justify-center">
-        <div class="bg-white flex flex-col items-center py-4" style="height: 90%; width: 90%">
-            <div class="text-xl font-bold">Client Registration</div>
+    <div class="w-full flex flex-col items-center justify-center bg-sky-100 bg-sky-300 p-5">
+        <div class="flex flex-col items-center p-4 bg-white" style="border-radius: 20px">
+            <h1 class="h1 fw-bold mb-3 text-uppercase">Client Registration</h1>
+
+            <form action="" method="POST" enctype="multipart/form-data">
+                @csrf
+                <!--------------Laundry Owner Information------------->
+                <h1 class="h4 p-2 text-center fw-bold">Laundry Owner Information</h1>
+                <div class="row px-3">
+                    <div class="col-md-4 mb-3">
+                        <label for="" class="fw-bold">First Name</label>
+                        <input value="{{ old('fname') }}" type="text" name="fname"
+                            class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" />
+                    </div>
+                    <div class="col-md-4 mb-3">
+                        <label for="" class="fw-bold">Middle Name</label>
+                        <input value="{{ old('mname') }}" type="text" name="mname"
+                            class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" />
+                    </div>
+                    <div class="col-md-4 mb-3">
+                        <label for="" class="fw-bold">Last Name</label>
+                        <input value="{{ old('lname') }}" type="text" name="lname"
+                            class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" />
+                    </div>
+                </div>
+
+                <div class="row px-3">
+                    <div class="col-md-4 mb-3">
+                        <label for="" class="fw-bold">Birthday</label>
+                        <input value="{{ old('bday') }}" type="text" name="bday"
+                            class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" />
+                    </div>
+                    <div class="col-md-4 mb-3">
+                        <label for="" class="fw-bold">Gender</label>
+
+                        <select name="" id=""
+                            class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1">
+                            <option disabled selected>Select your gender</option>
+                            <option value="Male">Male</option>
+                            <option value="Female">Female</option>
+                        </select>
+                    </div>
+                    <div class="col-md-4 mb-3">
+                        <label for="" class="fw-bold">Cellphone Number</label>
+                        <input value="{{ old('cnum') }}" type="text" name="cnum"
+                            class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" />
+                    </div>
+                </div>
+                <!-------------- End of Laundry Owner Information------------->
+
+                <!--------------Laundry Shop Account------------->
+                <h1 class="h4 p-2 text-center mt-3 fw-bold">Laundry Shop Account</h1>
+                <div class="row px-3">
+                    <div class="col-md-6 mb-3">
+                        <label for="" class="fw-bold">Username</label>
+                        <input value="{{ old('uname') }}" type="text" name="uname"
+                            class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" />
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <label for="" class="fw-bold">Email</label>
+                        <input value="{{ old('email') }}" type="email" name="email"
+                            class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" />
+                    </div>
+                </div>
+
+                <div class="row px-3">
+                    <div class="col-md-6 mb-3">
+                        <label for="" class="fw-bold">Password</label>
+                        <input value="{{ old('pwd') }}" type="password" name="pwd"
+                            class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" />
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <label for="" class="fw-bold">Confirm Password</label>
+                        <input value="{{ old('confpw') }}" type="password" name="confpw"
+                            class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" />
+                    </div>
+                </div>
+                <!-------------- End of Laundry Shop Account------------->
+
+                <!--------------Laundry Owner's Address------------->
+                <h1 class="h4 p-2 text-center mt-3 fw-bold">Laundry Owner's Address</h1>
+                <div class="row px-3">
+                    <div class="col-md-4 mb-3">
+                        <label for="" class="fw-bold">Region</label>
+                        <select id="region"
+                            class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"></select>
+                    </div>
+                    <div class="col-md-4 mb-3">
+                        <label for="" class="fw-bold">State/Province</label>
+                        <select id="province"
+                            class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"></select>
+                    </div>
+                    <div class="col-md-4 mb-3">
+                        <label for="" class="fw-bold">City</label>
+                        <select id="city"
+                            class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"></select>
+                    </div>
+                </div>
+
+                <div class="row px-3">
+                    <div class="col-md-6 mb-3">
+                        <label for="" class="fw-bold">Barangay</label>
+                        <select id="barangay"
+                            class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"></select>
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <label for="" class="fw-bold">House No.</label>
+                        <input value="{{ old('houseNo') }}" type="text" name="houseNo"
+                            class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" />
+                    </div>
+                </div>
+                <!--------------End of Laundry Owner's Address------------->
+
+                <!--------------Laundry Shop Information------------->
+                <h1 class="h4 p-2 text-center mt-3 fw-bold">Laundry Shop Information</h1>
+                <div class="row px-3">
+                    <div class="col-md-6 mb-3">
+                        <label for="" class="fw-bold">Laundry Shop Name</label>
+                        <input value="{{ old('laundryName') }}" type="text" name="laundryName"
+                            class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" />
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <label for="" class="fw-bold">Type of Laundry Shop</label>
+                        <input value="{{ old('typeLaundry') }}" type="text" name="typeLaundry"
+                            class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" />
+                    </div>
+                </div>
+
+                <div class="row p-3">
+                    <div class="col-md-4 mb-3">
+                        <label for="" class="fw-bold">Region</label>
+                        <select id="region"
+                            class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"></select>
+                    </div>
+                    <div class="col-md-4 mb-3">
+                        <label for="" class="fw-bold">State/Province</label>
+                        <select id="province"
+                            class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"></select>
+                    </div>
+                    <div class="col-md-4 mb-3">
+                        <label for="" class="fw-bold">City</label>
+                        <select id="city"
+                            class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"></select>
+                    </div>
+                </div>
+
+                <div class="row px-3">
+                    <div class="col-md-6 mb-3">
+                        <label for="" class="fw-bold">Barangay</label>
+                        <select id="barangay"
+                            class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"></select>
+
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <label for="" class="fw-bold">House No.</label>
+                        <input value="{{ old('houseNo') }}" type="text" name="houseNo"
+                            class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" />
+                    </div>
+                </div>
+                <!--------------End of Laundry Shop Information------------->
+
+
+                <!--------------Laundry Shop Documents------------->
+                <h1 class="h4 p-2 text-center mt-3 fw-bold">Laundry Shop Documents</h1>
+                <div class="row px-3">
+                    <div class="col-md-6 mb-3">
+                        <label for="" class="fw-bold">BIR Certificate of Registration</label>
+                        <input type="file"
+                            class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" />
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <label for="" class="fw-bold">Business Permit / Mayor Permit</label>
+                        <input type="file"
+                            class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" />
+                    </div>
+                </div>
+                <div class="row px-3">
+                    <div class="col-md-6 mb-3">
+                        <label for="" class="fw-bold">DTI Business Registration</label>
+                        <input type="file"
+                            class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" />
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <label for="" class="fw-bold">Valid ID of Laundry Owner</label>
+                        <input type="file"
+                            class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" />
+                    </div>
+                </div>
+                <!--------------End of Laundry Shop Documents------------->
+
+
+                <div class="d-flex justify-content-center align-items-center flex-column mb-3">
+                    <div class="check-box mb-3 mt-3 ">
+                        <input type="checkbox" name="" id="" class="mt-1 px-3 py-2 bg-white border">
+                        <label for="" class="fw-bold">I have read and agree to the terms and conditions</label>
+                    </div>
+                    <button class="btn btn-block text-white btn-lg " style="background-color: #FFD580"
+                        type="submit">Register</button>
+                </div>
+
+
+            </form>
 
             {{-- <select id="region"></select> ito na yung fields na kailangan for API ng address. Need nalang ng styling
             <select id="province"></select>
